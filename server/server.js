8492e20cb55448f08052cdd728449f2c5a4f4c4a
Christopher Black <chris@primeacademy.io>
2023-03-01 21:35:15 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5001;
 const koalaRouter = require('./routes/koala.router')
 
