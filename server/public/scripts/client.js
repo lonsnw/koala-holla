1391937872da14eb836761a4c26e2376b84e9539
lons <lonsnw@gmail.com>
2024-03-16 10:58:02 -0500

    Updating with client side PUT request

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -44,36 +44,14 @@ function getKoalas(){
 } // end getKoalas
 
 // function for changing transfer status
-function transferKoala(event) {
+function transferKoala(koalaId) {
   console.log('in transfer koala');
-  event.preventDefault();
-  // this changes it only on the DOM.
-  event.target.parentElement.innerHTML = `<td>Updating</td>`;
-  // seems like maybe i need to get the ID number to update the object? 
-  // i think this is going to be a PUT request.
-  // -------------------------------------------------------------
-  // i tried the below and it was not the solution.
-  // let koalaName = event.target.parentElement.document.querySelector('#nameIn').value;
-  // let koalaAge = event.target.parentElement.document.querySelector('#ageIn').value;
-  // let koalaColor = event.target.parentElement.document.querySelector('#colorIn').value;
-  // let koalaTransfer = event.target.parentElement.document.querySelector('#readyForTransferIn').value;
-  // let koalaNotes = event.target.parentElement.document.querySelector('#notesIn').value;
-  // let koala = {
-  //   name: koalaName,
-  //   age: koalaAge,
-  //   color: koalaColor,
-  //   transfer: koalaTransfer,
-  //   notes: koalaNotes,
-  // }
-  // axios.post('/koalas/transfers', koala).then((response => {
-  //   console.log(response);
-  //   // function that will display all koalas in the table
-  //   getKoalas();
-  // })).catch((error) => {
-  //   console.error(error);
-  // });
-  // // event.target.parentElement.parentElement
-  // // getKoalas();
+  axios.put(`/koalas/transfer/${koalaId}`).then((response) => {
+    getKoalas();
+  }).catch((error) => {
+    console.log('Error in PUT', error);
+    alert(`Something went wrong updating this koala's status`);
+  });
 }
 
 
